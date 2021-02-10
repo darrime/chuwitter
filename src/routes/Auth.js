79ca22c4f7c0165ffc1f3603d9658ad5c144835e
dongchu627 <dongchu627@github.com>
2021-02-10 14:42:45 +0900

    Firebase에 user 저장

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,31 +1,53 @@
+import { authService } from "fbase";
 import react, { useState } from "react";
+import React from "react";
 
 const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const onChange = (event) => {
-        const {target: {name, value}} =event;
-        if(name == "email") {
-            setEmail(value)
-        } else if(name == "password"){
-            setPassword(value);
-        }
-    };
-    const onSubmit = (event) => {
-        event.preventDefault();
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name == "email") {
+      setEmail(value);
+    } else if (name == "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+        let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(
+          email,
+          password
+        );
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
-      <from onSubmit={onSubmit}>
+      <form onSubmit={onSubmit}>
         <input
-        name = "email" 
-        type="text"
-        placeholder="Email" 
-        required 
-        value={email}
-        onChange={onChange}
-        />    
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
 
         <input
           name="password"
@@ -33,13 +55,14 @@ const Auth = () => {
           placeholder="password"
           required
           value={password}
-          onChange ={onChange}
-          /> 
+          onChange={onChange}
+        />
 
-        <input 
-        type="submit" 
-        value="Log in" />
-      </from>
+        <input
+          type="submit"
+          value={newAccount ? "계정을 만드세요" : "로그인하세요"}
+        />
+      </form>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
